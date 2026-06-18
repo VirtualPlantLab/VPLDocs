8e25c6af475ab358df24c0578aefae78813cf84f
Documenter.jl <documenter@juliadocs.github.io>
2026-06-18 07:39:38 +0000

    build based on 47900ac

--- dev/build_doc.jl
+++
@@ -1,1 +0,0 @@
-julia --project=docs/ -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.instantiate()'
--- versions.js
+++ versions.js
@@ -1,7 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
   "v1.0",
-  "v0.0",
   "dev",
 ];
 var DOCUMENTER_NEWEST = "v1.0.1";
